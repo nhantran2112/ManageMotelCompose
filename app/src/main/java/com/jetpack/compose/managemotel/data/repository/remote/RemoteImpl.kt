e5473b224caa440d32e807c251ed1ba2fb4c1a66
ttnhan211202 <ttnhan211202@gmail.com>
2023-06-12 11:22:59 +0700

    test

--- app/src/main/java/com/jetpack/compose/managemotel/data/repository/remote/RemoteImpl.kt
+++ app/src/main/java/com/jetpack/compose/managemotel/data/repository/remote/RemoteImpl.kt
@@ -1,11 +1,9 @@
 package com.jetpack.compose.managemotel.data.repository.remote
 
-import android.util.Log
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
-import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 import com.jetpack.compose.managemotel.data.model.Room
 
@@ -13,7 +11,7 @@ class RemoteImpl : Remote {
     private val database = Firebase.database.reference.child("data")
 
     override suspend fun getData(): List<Room> {
-        database.addValueEventListener(object : ValueEventListener{
+        database.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 val value = snapshot.value
             }
